7397d8c383ca26534a21451a8926c89b0a521946
Justin Bernstein <jmbernst223@gmail.com>
2023-05-22 22:46:47 -0500

    Create treePL_Fresh.sh

---
+++ treePL/treePL_Fresh.sh
@@ -0,0 +1,4 @@
+#To run treePL, you just run the following:
+treePL Homalopsidae_Fresh_NoGEO.config
+
+#Then run random subsample and replicate cross-validation (RSRCV) by adding 'randomcv' at the end of the configuration file
