51a28ef65adcf2d8fba859865370c0b67861f652
Justin Bernstein <jmbernst223@gmail.com>
2023-05-22 22:15:47 -0500

    Update ASTRAL_FreshSamples.sh

--- ASTRAL-III/ASTRAL_FreshSamples.sh
+++ ASTRAL-III/ASTRAL_FreshSamples.sh
@@ -1,2 +1,3 @@
 #THe below code is the script to run ASTRAL-III
-java -jar astral.5.7.8.jar -i ALL-gene-trees.contree -o Homalopsidae_75p_ALLLoci_FreshOnly.tre
+# -o creates the output
+java -jar astral.5.7.8.jar -i ALL-gene-trees.contree -o Homalopsidae_75p_ALLLoci_ATRAL_FreshOnly.tre
